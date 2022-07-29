c03c77d885e81e72b92b29fd771a75e414d51e51
Johannes Hörner <johannes.hoerner@univie.ac.at>
2022-07-29 16:34:50 +0200

    fixed app-snowball-bars.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:server
+web: gunicorn app-snowball-bars:server
--- app-snowball-bars.py
+++ app-snowball-bars.py
@@ -9,6 +9,8 @@ import numpy as np
 
 app = Dash(__name__)
 
+server = app.server
+
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 DS = xr.open_dataset("data/ape_5000_55_0S_atm_2d_ml.ym.zm.nc",decode_times=False)
